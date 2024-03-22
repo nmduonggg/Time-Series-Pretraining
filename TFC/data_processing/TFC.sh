0a6152bbde08acf6ede6d3646bb192f68c24302d
nmduonggg <nmduong2104@gmail.com>
2024-03-22 13:37:47 +0700

    update code

--- TFC/data_processing/TFC.sh
+++
@@ -1,9 +0,0 @@
-mkdir code/TFC/data/{SleepEEG,Epilepsy,FD-A,FD-B,HAR,Gesture,ECG,EMG}
-ln -s datasets/SleepEEG/{train,val,test}.pt code/TFC/data/SleepEEG/
-ln -s datasets/Epilepsy/{train,val,test}.pt code/TFC/data/Epilepsy/
-ln -s datasets/FD-A/{train,val,test}.pt code/TFC/data/FD-A/
-ln -s datasets/FD-B/{train,val,test}.pt code/TFC/data/FD-B/
-ln -s datasets/HAR/{train,val,test}.pt code/TFC/data/HAR/
-ln -s datasets/Gesture/{train,val,test}.pt code/TFC/data/Gesture/
-ln -s datasets/ECG/{train,val,test}.pt code/TFC/data/ECG/
-ln -s datasets/EMG/{train,val,test}.pt code/TFC/data/EMG/
--- data_processing/ECG/code/reduce.sh
+++
@@ -1,4 +0,0 @@
-python reduct_leads_wfdb.py  \
-    -i /mnt/disk4/nmduong/Time-Series-Pretraining/data_processing/ECG/RAW_DATA/files/challenge-2021/1.0.3/training/chapman_shaoxing/g1  \
-    -o /mnt/disk4/nmduong/Time-Series-Pretraining/data_processing/ECG/PROCESSED/chapman_shaoxing/g1 \
-    -l I II III aVR aVL aVF 
